26764eb13d82da698b00ed8d8ef207ef414c5c2e
tvtvtv9810 <tvtvtv9810@gmail.com>
2022-09-11 02:00:54 +0900

    いろいろ修正

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+
 var app = express();
 
+// express.static ミドルウェア関数
+app.use(express.static('static'));
 // View EngineにEJSを指定。
 app.set('view engine', 'ejs');
 
-var server = app.listen(3000, function(){
+const port = 3000;
+
+var server = app.listen(port, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
 
 // "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
 app.get("/", function(req, res, next){
-    res.render("index", {});
+    res.render("top", {});
 });
